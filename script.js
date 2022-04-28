e6362cc643823c459092a3e53470b1c1ffa8e73e
RCvetinskis <robertas.cvetinskis@gmail.com>
2022-04-28 10:35:39 +0300

    pridejom burger rotate, ir pasalinom side navbar su window resize

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ burger.addEventListener("click",function(){
     }
 
     sideMenu.classList.toggle("translate")
+    this.classList.toggle("rotate")
+})
+window.addEventListener("resize", function(){ // resize eventlistener veikia kaip media query
+    if(this.window.innerWidth > 760) // priskiari jog funkcija veiktu kai width yra  daugiau760px 
+    for(var x of anchors){
+        x.classList.remove("active") // kai 760px ar daugiau nuimam burger ir side menu
+  
+    sideMenu.classList.remove("translate")
+    this.classList.remove("rotate")
+}
+
 })
